867c00236fecb4336b35da68ddfff463059bb084
DandelionOnDONBASS <109672647+DandelionOnDONBASS@users.noreply.github.com>
2022-10-15 11:37:01 +0300

    add lesson2.2_step8.py

---
+++ lesson2.2_step8.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+import os
+
+
+
+link = "http://suninjuly.github.io/file_input.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.NAME, "firstname")
+    input1.send_keys('Ivan')
+
+    input2 = browser.find_element(By.NAME, "lastname")
+    input2.send_keys('Vasichkin')
+
+    input3 = browser.find_element(By.NAME, "email")
+    input3.send_keys('Vasichkin@gmail.com')
+
+
+
+
+
+    file_input = browser.find_element(By.ID, 'file')
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(current_dir, 'file.txt')
+    file_input.send_keys(file_path)
+
+
+
+    button = browser.find_element(By.XPATH, '//button[@type="submit"]')
+    button.click()
+
+
+
+
+finally:
+
+    time.sleep(10)
+
+    browser.quit()
